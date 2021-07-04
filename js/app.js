1817f2d6cb49307f435ebe0ce852327e074d5d54
crogmi <chrisogami16@gmail.com>
2021-07-03 20:31:13 -0700

    Updated main section for HTML and CSS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".menu-links");
-const links = document.querySelector(".menu-links li");
+const links = document.querySelector(".menu-items");
 
 hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+})
+
+// Meed to understand what is causing the error on the About and Contact links
+
+links.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 })
